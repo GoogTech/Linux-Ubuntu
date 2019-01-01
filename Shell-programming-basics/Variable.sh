97ea4fd0c868df77dd5e2f9292c570d0b0c403ab
HuangYuhui <Gentleman_0109@outlook.com>
2019-01-01 19:15:25 +0800

    Learn to write Shell program ~

---
+++ Shell-programming-basics/Variable.sh
@@ -0,0 +1,12 @@
+#Learn to use the variable about the script
+
+#Definde the variable of A
+declare -i A=100
+echo "A = $A"
+echo "-- unset the parameter of A --"
+unset A
+echo "A = $A "
+
+#Defind the stirng type of variable
+declare var="Hi Shell"
+echo "$var"
