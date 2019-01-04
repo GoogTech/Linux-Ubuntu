ec36ff07fb4d97b898bb96f03062bde1dbfb7027
HuangYuhui <Gentleman_0109@outlook.com>
2019-01-04 13:58:36 +0800

    Learn more command about Shell ~

--- Shell-programming-basics/Variable.sh
+++ Shell-programming-basics/Variable.sh
@@ -10,3 +10,51 @@ echo "A = $A "
 #Defind the stirng type of variable
 declare var="Hi Shell"
 echo "$var"
+
+#Indirect reference of variales
+message=hello
+hello="Good Morning"
+echo "message : $message"
+echo "hello : $hello"
+echo "The result of first method are as follows :"
+echo "${!message}"
+echo "THe result of second method are as follows : "
+eval message=\$$message
+echo $message
+
+#Substitution of special variables.
+#{var:-value}
+var=
+echo "hello,${var:-world}"
+var="Shell"
+echo "hello,${var:-world}"
+#{var:=value}
+var=
+echo "hello,${var:=World}"
+var="Shell"
+echo "hello,${var:=World}"
+#{var:?value}
+var="VariableIsNullException"
+echo "hello,${var:?value}"
+var=
+#It's will throw a default exception so that the program will be stop !
+#echo "helllo,${var:?value}"
+
+#Single quotes and double quotes.
+var="Hi Shell ~"
+echo $var
+var='Hi Shell ~'
+echo $var
+var="Shell ~"
+echo "Hi,${var}"
+echo 'Hi,${var}'
+
+#Command replacement.
+#First Form : `command`
+todayDate=`date`
+echo $todayDate
+#Second Form : (command)
+files=$(ls)
+echo $files
+
+
